f1f3b1cc6a6c4534bad59f015072c323a5721cac
Tom Morgan <tsmorgan@gmail.com>
2023-04-25 18:58:27 +0100

    Updates to make it do what I want ;-)

--- scripts/index.js
+++ scripts/index.js
@@ -54,31 +54,77 @@ chrome.runtime.onMessage.addListener(
       links = randomSample(links, 10);
       var cont = document.getElementById('content');
       cont.innerHTML = '';
-      for (var c in links) {
-        var linkDiv = document.createElement('div');
-        var desc = document.createElement('a');
-        var br = document.createElement('br');
-        desc.className = 'link-desc';
-        desc.innerHTML = links[c].description;
-        linkDiv.appendChild(desc);
-        linkDiv.appendChild(br);
-        var linkElement = document.createElement('a');
-        linkElement.className = "pin-link";
-        if (links[c].href.length > 50) {
-          linkElement.innerHTML = links[c].href.slice(0, 40) + '...';
-        } else {
-          linkElement.innerHTML = links[c].href;
-        }
-        linkElement.href = links[c].href;
-        desc.href = links[c].href;
-        desc.target = '_blank';
-        linkElement.target = '_blank';
-        linkDiv.appendChild(linkElement);
-        // cont.appendChild(br);
-        cont.appendChild(linkDiv);
-      }
+      var cont2 = document.getElementById('content2');
+      cont2.innerHTML = '';
+      buildDiv(cont,links.slice(0,5));
+      buildDiv(cont2,links.slice(5,10));      
     }
     sendResponse({});
     return true;
   }
 );
+
+function buildDiv(cont,links)
+{
+  console.log(links);
+  
+  for (var c in links) {
+
+    var linkDiv = document.createElement('div');
+    linkDiv.className = 'link-div';
+
+    var desc = document.createElement('a');
+    var br = document.createElement('br');
+    desc.className = 'link-desc';
+    desc.innerHTML = links[c].description;
+
+    linkDiv.appendChild(desc);
+    linkDiv.appendChild(br);
+
+    var extra = document.createElement('p');
+    extra.className = 'link-extra';
+    extra.innerHTML = links[c].extended;
+
+    linkDiv.appendChild(extra);
+
+    console.log(links[c].hash);
+    console.log(links[c].hash.slice(0,12));
+
+    // https://pinboard.in/u:ghijklmno/b:3eb92143348a
+    // 1ed11c04c293ebf3a3592fd3be4262bc
+    // 654aeecd9a719bae345cb7dbedc1f986
+    // 654aeecd9a719bae345cb7dbedc1f986
+    https://pinboard.in/u:ghijklmno/b:654aeecd9a71
+
+    var br2 = document.createElement('br');
+    var edit = document.createElement('a');
+    edit.className = "pin-link";
+    edit.target = '_blank';
+    edit.innerHTML = 'edit';
+    edit.href = "https://pinboard.in/u:ghijklmno/b:"+links[c].hash.slice(0,12);
+
+    // "https://pinboard.in/search/u:ghijklmno?query="+links[c].description;
+    // 
+
+    linkDiv.appendChild(edit);
+
+    var spacer = document.createElement('span');
+    spacer.innerHTML = ' | ';
+    linkDiv.appendChild(spacer);
+
+    var linkElement = document.createElement('a');
+    linkElement.className = "pin-link";
+    if (links[c].href.length > 50) {
+      linkElement.innerHTML = links[c].href.slice(0, 40) + '...';
+    } else {
+      linkElement.innerHTML = links[c].href;
+    }
+    linkElement.href = links[c].href;
+    desc.href = links[c].href;
+    desc.target = '_blank';
+    linkElement.target = '_blank';
+
+    linkDiv.appendChild(linkElement);
+    cont.appendChild(linkDiv);
+  }
+}
